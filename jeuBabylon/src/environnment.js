a1ba82b5c5181103f337c3e9d2d505eba81d4507
Raphaël Guichet <raphael.guichet@etu.unice.fr>
2026-03-06 13:52:54 +0100

    ajout skybox

--- jeuBabylon/src/environnment.js
+++ jeuBabylon/src/environnment.js
@@ -1,9 +1,7 @@
-import {
-  MeshBuilder,
-  PhotoDome,
-  StandardMaterial,
-  Texture,
-} from "@babylonjs/core";
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
+import { Texture } from "@babylonjs/core/Materials/Textures/texture";
+import { CubeTexture } from "@babylonjs/core/Materials/Textures/cubeTexture";
 
 export class Environment {
   constructor(scene) {
@@ -11,7 +9,6 @@ export class Environment {
   }
 
   async load() {
-    // create a flat ground
     const ground = MeshBuilder.CreateGround(
       "ground",
       { width: 24, height: 24 },
@@ -19,15 +16,18 @@ export class Environment {
     );
 
     const groundMaterial = new StandardMaterial("groundMat", this.scene);
-    groundMaterial.diffuseTexture = new Texture("ground.jpg", this.scene);
+    groundMaterial.diffuseTexture = new Texture("/ground.jpg", this.scene);
     ground.material = groundMaterial;
 
-    // sky / photo dome
-    const skyDome = new PhotoDome(
-      "skyDome",
-      "Sky1.jpg",
-      { resolution: 32, size: 500 },
-      this.scene,
-    );
+    const skybox = MeshBuilder.CreateBox("skyBox", { size: 100.0 }, this.scene);
+    const skyboxMaterial = new StandardMaterial("skyBox", this.scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
+    skybox.infiniteDistance = true;
+    skyboxMaterial.disableLighting = true;
+    const cubeTex = new CubeTexture("/textures/skybox/skybox", this.scene);
+    skyboxMaterial.reflectionTexture = cubeTex;
+    skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
   }
 }
