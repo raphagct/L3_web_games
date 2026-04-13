6bb975139e560ac02596099b395eaba32fd16c6a
Mathis LECHEVALIER <mathis.lechevalier@etu.unice.fr>
2026-04-13 09:57:39 +0200

    test amélioration vercel

--- jeuBabylon/vite.config.js
+++ jeuBabylon/vite.config.js
@@ -2,4 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  build: {
+    // Augmente la limite pour éviter les warnings bloquants
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        // Force la séparation des bibliothèques externes (Babylon) du code de ton jeu
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
